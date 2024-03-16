860f821790886c65bf1417197ef445ef865c3f5f
Euan Dovey-McKay <euan.dovey-mckay@hotmail.com>
2024-03-16 21:04:53 +0000

    Updated turtle butterfly curve drawing

--- Challenge 1/Drawing Butterfly curve in turtle.py
+++ Challenge 1/Drawing Butterfly curve in turtle.py
@@ -6,12 +6,13 @@ Colours = ["#e81416","#ffa500","#faeb36","#79c314","#487de7","#4b369d","#70369d"
 
 wn = turtle.Screen()
 wn.screensize(1920,1080)
+wn.tracer(0)
 
 t = np.linspace(0,12*np.pi,1000)
 # According to WikiPedia, t needs to be between 0 and 12pi
 # The last arg (1000) is the number of points it plots to (essentially the slots it lin spaces out to)
 
-xBFly = 197*(np.sin(t) * ( (np.exp(np.cos(t)) ) - ( 2*np.cos(4*t) ) - ( (np.sin(t/12))**5 ) )) - 125
+xBFly = 197*(np.sin(t) * ( (np.exp(np.cos(t)) ) - ( 2*np.cos(4*t) ) - ( (np.sin(t/12))**5 ) ))
 yBFly = 197*(np.cos(t) * ( (np.exp(np.cos(t)) ) - ( 2*np.cos(4*t) ) - ( (np.sin(t/12))**5 ) )) - 125
 # Using parametric equations from WikiPedia: https://en.wikipedia.org/wiki/Butterfly_curve_(transcendental)
 # I had to multiply by 197 to make it larger as to fit the whole of a 1920x1080 monitor, and I also took it down by 125 pixels to fit onto the screen (wings went off otherwise)
@@ -35,4 +36,5 @@ for i in range(len(xBFly)):
     # The MOD 7 is so that the index never exceeds the possible list indexes (1-6)... it basically is just to stop index errors
     Pen.color(Colours[ColourIndex])
 
+wn.update()
 wn.mainloop()
